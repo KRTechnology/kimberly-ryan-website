554af444e5f4724ad1a35c3ad794b3ac1f76b69e
chemazu <chemazu>
2025-11-21 10:36:04 +0100

    addition of google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Inter, IBM_Plex_Sans } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -20,11 +21,28 @@ const ibmPlexSans = IBM_Plex_Sans({
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
+}) {
   return (
     <html lang="en">
+      <head>
+        {/* Google Analytics */}
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-66ZZW3JKGS"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-66ZZW3JKGS');
+          `}
+        </Script>
+      </head>
+
       <body className={`${inter.variable} ${ibmPlexSans.variable}`}>
         {children}
       </body>
