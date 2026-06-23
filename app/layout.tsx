4331d326bf03434935db5486037341ed7193bed0
Kimberly Ryan Limited <79824797+KRTechnology@users.noreply.github.com>
2026-06-23 13:44:58 +0100

    Add LinkedIn Insight Tag to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,10 +41,35 @@ export default function RootLayout({
             gtag('config', 'G-66ZZW3JKGS');
           `}
         </Script>
+
+        {/* LinkedIn Insight Tag */}
+        <Script id="linkedin-partner-id" strategy="afterInteractive">
+          {`
+            _linkedin_partner_id = "8313025";
+            window._linkedin_data_partner_ids = window._linkedin_data_partner_ids || [];
+            window._linkedin_data_partner_ids.push(_linkedin_partner_id);
+          `}
+        </Script>
+
+        <Script
+          id="linkedin-insight"
+          strategy="afterInteractive"
+          src="https://snap.licdn.com/li.lms-analytics/insight.min.js"
+        />
       </head>
 
       <body className={`${inter.variable} ${ibmPlexSans.variable}`}>
         {children}
+
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            alt=""
+            src="https://px.ads.linkedin.com/collect/?pid=8313025&fmt=gif"
+          />
+        </noscript>
       </body>
     </html>
   );
